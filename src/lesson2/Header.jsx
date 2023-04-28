261ffa569983cd51d170ea46b5f4a96a7e30921d
normuhamedov <normuhamedov22@gmail.com>
2023-04-28 23:17:45 +0500

    abcd

--- src/lesson2/Header.jsx
+++ src/lesson2/Header.jsx
@@ -1,15 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { MyContext } from './CounterExample';
 import { CiDark } from 'react-icons/ci'
 import { MdOutlineLightMode } from 'react-icons/md'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { FiDelete } from 'react-icons/fi'
 import './Figm.scss'
 import './../App.css';
 import logo from "./img/logo.png"
 const Header = () => {
-
+  
   const {counter, setCounter} = useContext(MyContext);
   
-
+  const [counterr, setCounterr] = useState(false);
+  const handleButtonClickr= () => {
+    setCounterr(!counterr);
+  };
   const handleButtonClick = () => {
     setCounter(!counter);
 
@@ -31,6 +36,20 @@ const Header = () => {
            <button className='btn btn-outline-light'>Login</button>
            </li>
           </ul>
+          <div className='dNone'>
+            <button className='btn-onclick' onClick={handleButtonClickr}>{counterr ?  <FiDelete /> : <RxHamburgerMenu/>}</button>
+            <div className={`burger_open  ${counterr ? 'burger_close' : ''}`}>
+            <ul className="header_open pt-5">
+           <li className='nav_li_open btn btn-outline-light'>Menu</li>
+           <li className='nav_li_open btn btn-outline-light'>News</li>
+           <li className='nav_li_open btn btn-outline-light'>About Us</li>
+           <li className='nav_li_open btn btn-outline-light'>Contact Us</li>
+           <li className='nav_li_open mt-5'>
+           <button className='btn btn-outline-light'>Login</button>
+           </li>
+          </ul>
+            </div>
+          </div>
         </div>
       </header>
     </>
